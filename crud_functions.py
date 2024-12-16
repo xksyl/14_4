d06bb77bfdb425ef9447d4645e803ea573348c57
xksyl <egorka2004212@gmail.com>
2024-12-17 01:43:08 +0500

    crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -1,12 +1,9 @@
 import sqlite3
 
-# Функция для инициализации базы данных (создание таблицы и добавление данных)
 def initiate_db():
-    # Подключаемся к базе данных (если базы нет, она будет создана)
     connection = sqlite3.connect('Products.db')
     cursor = connection.cursor()
 
-    # Создаем таблицу (если она еще не существует)
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Products (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,29 +13,24 @@ def initiate_db():
     )
     ''')
 
-    # Добавляем продукты в таблицу, если она пустая
     cursor.execute('''
     SELECT COUNT(*) FROM Products
     ''')
     result = cursor.fetchone()
 
-    # Если таблица пуста, добавляем начальные данные
     if result[0] == 0:
         add_product('Logitech G102', 'Мышь с сенсором 8000 DPI', 10000)
         add_product('Logitech G304', 'Беспроводная игровая мышь', 15000)
         add_product('Logitech G PRO', 'Мышь для профессиональных игроков', 20000)
         add_product('ARDOR GAMING Fury', 'Игровая мышь с подсветкой', 25000)
 
-    # Сохраняем изменения и закрываем соединение
     connection.commit()
     connection.close()
 
-# Функция для добавления продукта в таблицу
 def add_product(title, description, price):
     connection = sqlite3.connect('Products.db')
     cursor = connection.cursor()
 
-    # Вставляем продукт в таблицу
     cursor.execute('''
     INSERT INTO Products (title, description, price) VALUES (?, ?, ?)
     ''', (title, description, price))
@@ -46,7 +38,6 @@ def add_product(title, description, price):
     connection.commit()
     connection.close()
 
-# Функция для получения всех продуктов из базы данных
 def get_all_products():
     connection = sqlite3.connect('Products.db')
     cursor = connection.cursor()
